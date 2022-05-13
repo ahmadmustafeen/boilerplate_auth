c15b007b631885d76fd55169dccf488e72ef6959
Ahmad <engr.ahmadmustafeen@gmail.com>
2022-05-13 17:08:41 +0500

    signin screen lint issue resolved

--- src/containers/auth/signin/index.js
+++ src/containers/auth/signin/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, FormControl, InputGroup } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { InputWithLabel } from "../../../components";
 import "./style.css";
