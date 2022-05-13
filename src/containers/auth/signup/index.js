91abc15955f145a821373716d2b6e44735165777
Ahmad <engr.ahmadmustafeen@gmail.com>
2022-05-13 17:07:03 +0500

    signup screen ui completed

--- src/containers/auth/signup/index.js
+++ src/containers/auth/signup/index.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Button, } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { InputWithLabel } from "../../../components";
 
 const SignUp = () => {
   return (
-    <div>
-        <h2>Sign up</h2>
+    <div className="overlay">
+      <div className="screen-container">
+        <div className="screen-content">
+          <div className="screen-content-header">
+            <h1>Sign Up</h1>
+          </div>
+          <div className="screen-content-body">
+            <div className="form-container">
+              <form>
+                <InputWithLabel label="First Name" />
+                <InputWithLabel label="Last Name" />
+                <InputWithLabel label="Phone" />
+                <InputWithLabel label="Email" />
+                <InputWithLabel label="Password" />
+                <InputWithLabel label="Confirm Password" />
+                <div className="submitbuttoncontainer-login">
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    style={{ width: "50%" }}
+                  >
+                    Sign In
+                  </Button>
+                </div>
+              </form>
+            </div>
+            <div className="signup-container">
+              <p>
+                Already have an account?{" "}
+                <span className="signup-link">
+                <Link to="/login">Sign In</Link>
+                </span>
+              </p>
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
