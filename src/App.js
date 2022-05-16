1602f2e893772f3d48c59cac660f40a2e81ef34e
Ahmad <engr.ahmadmustafeen@gmail.com>
2022-05-16 13:58:42 +0500

    removed unused import and comment from app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
-import ForgetPassword from './containers/auth/forgetpassword';
 import Navigator from './navigator';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Navigator />
-      {/* <ForgetPassword /> */}
     </BrowserRouter>
   );
 }
